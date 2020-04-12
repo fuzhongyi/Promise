83903136cef18ac14160ab4c51c3c8aef53ca506
fuzhongyi <fuzhongyi@doctorwork.com>
2020-04-12 23:52:53 +0800

    feat: Promise.allSettled

--- src/promise.js
+++ src/promise.js
@@ -189,6 +189,31 @@ Promise.all = function (promises) {
   });
 }
 
+Promise.allSettled = function (promises) {
+  return new Promise((resolve, reject) => {
+    let index = 0;
+    let result = [];
+    if (promises.length === 0) {
+      resolve(result);
+    } else {
+      function processValue(i, data) {
+        result[i] = data;
+        if (++index === promises.length) {
+          resolve(result);
+        }
+      }
+      for (let i = 0; i < promises.length; i++) {
+        //promises[i] 可能是普通值
+        Promise.resolve(promises[i]).then((data) => {
+          processValue(i, { status: 'fulfilled', value: data });
+        }, (err) => {
+          processValue(i, { status: 'rejected', reason: err })
+        });
+      }
+    }
+  });
+}
+
 Promise.race = function (promises) {
   return new Promise((resolve, reject) => {
     if (promises.length === 0) {
